22ec7a4a53fc00a88ee8bed9f0187c0c153e0046
Random76520 <tun45218@temple.edu>
2023-04-13 12:06:01 -0400

    It should be able to work properly.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -10,20 +10,51 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binder: TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object: ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            binder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.playButton).setOnClickListener {
+        bindService (
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) {
+                binder.start(100)
+            }
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) {
+                binder.pause()
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnected) {
+                binder.stop()
+            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
     }
 }
